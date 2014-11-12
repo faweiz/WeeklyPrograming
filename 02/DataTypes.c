fdd709fbe713771f324b2c972bf4f3b90532493e
Faweiz <faweiz@gmail.com>
2014-11-11 23:44:22 -0500

    Data Types
    02: Data Types

---
+++ 02/DataTypes.c
@@ -0,0 +1,9 @@
+//C语言：每周1程序02: Data Types
+#include <stdio.h>
+int main(){
+	printf("size for int %d\n",sizeof(int));
+	printf("size for char %d\n",sizeof(char));
+	printf("size for double %d\n",sizeof(double));
+	printf("size for float %d\n",sizeof(float));
+	return 0;
+}
