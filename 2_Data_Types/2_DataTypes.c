118cf995090a3cb73b09d85667021ebcdfa0f837
Faweiz <faweiz@gmail.com>
2014-11-17 03:10:12 -0500

    11/17/2014

--- 2_Data_Types/2_DataTypes.c
+++ 2_Data_Types/2_DataTypes.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 int main(){
 	char a;
+	double i[10];
 	printf("/*******************************************************************/\n");
 	printf("sizeof(char) = %d\n",sizeof(char));
 	printf("sizeof(a)    = %d\n",sizeof(a));
@@ -11,6 +12,7 @@ int main(){
 	printf("sizeof(float)= %d\n",sizeof(float));
 	printf("sizeof(double)= %d\n",sizeof(double));
 	printf("sizeof(10.00001)= %d\n",sizeof(10.00001));
+	printf("sizeof(double i[10]) = %d\n",sizeof(i));
 	printf("/*******************************************************************/\n");
 	return 0;
 }
