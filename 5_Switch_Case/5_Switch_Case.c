f83f545a01e16e1ff8ab3add5c18dec74fe36bfc
Faweiz <faweiz@gmail.com>
2014-12-10 13:20:23 -0500

    C:Weekly programming 05: Switch Case

---
+++ 5_Switch_Case/5_Switch_Case.c
@@ -0,0 +1,26 @@
+//C:Weekly programming 05: Switch Case
+//https://github.com/faweiz
+#include<stdio.h>
+int main()
+{
+	char grade;
+	printf("Enter your grade: ");
+	scanf("%c",&grade);
+	switch(grade)
+	{
+	case 'A' :
+      printf("Excellent!\n");
+      break;
+	case 'B':
+		printf("Good Job!\n");
+		break;
+	case 'C':
+		printf("You passed\n");
+		break;
+	default:
+		printf("Invalid Grade\n");
+	}
+	printf("Your grade is %c \n",grade);
+	return 0;
+}
+//http://www.faweiz.com
