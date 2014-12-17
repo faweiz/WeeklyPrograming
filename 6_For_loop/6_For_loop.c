40e7b02bc928cfc09c4a7ef3eeaa3b98f20e2014
Faweiz <faweiz@gmail.com>
2014-12-16 23:39:00 -0500

    6_For_loop

---
+++ 6_For_loop/6_For_loop.c
@@ -0,0 +1,18 @@
+//C:Weekly programming 06: For Loop
+//https://github.com/faweiz
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	printf("From 0 - 9\n");
+	for(i=0;i<10;i++){
+		printf("i = %d\n",i);
+	}
+	printf("From 9 - 0\n");
+	for(j=9;j>0;j--){
+		printf("j =%d\n",j);
+	}
+	return 0;
+}
+//http://www.faweiz.com
+	
