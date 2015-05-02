b8f41d6fd3336636c9dea0b8d31330ba48b89408
Faweiz <faweiz@gmail.com>
2015-05-02 11:26:57 -0400

    17. Find average of numbers

--- 17. Find average of a Number/main.c
+++ 17. Find average of a Number/main.c
@@ -1,21 +1,27 @@
+// C:Weekly programming 17: Find average of numbers
+// https://github.com/faweiz
+// https://portfolium.com/faweiz
+// https://www.linkedin.com/in/faweiz
+// Enter numbers then Find average of numbers, when enter "0" to exit program
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int number;
-    int sum = 0;
+    float number;
+    float sum = 0;
     int count = 0;
     printf("Enter a number: ");
-    scanf("%d",&number);
-        while(number != -1)
+    scanf("%f",&number);
+        while(number != 0)
         {
             sum += number;
             count++;
-            printf("Enter other number(-1 to exit): ");
-            scanf("%d",&number);
+            printf("Enter other number(0 to exit): ");
+            scanf("%f",&number);
         }
-        printf("Average = %f\n",1.0*sum/count);
+        printf("Average = %.3f\n",1.0*sum/count);
 
     return 0;
 }
