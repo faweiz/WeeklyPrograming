010cada2755bdbdde14e667a6f0e0e70475a6972
Faweiz <faweiz@gmail.com>
2015-05-02 10:57:45 -0400

    16. Count Number of Digit of an integer

--- 16. Count Number of Digit/main.c
+++ 16. Count Number of Digit/main.c
@@ -1,8 +1,22 @@
+// C:Weekly programming 16: Count Number of Digit
+// https://portfolium.com/faweiz
+// https://www.linkedin.com/in/faweiz
+// Enter a integer  number and Count Number of Digit of this integer
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int x,count=0;
+    printf("Enter an integer number: ");
+    scanf("%d",&x);
+    int num = x;
+        while(x!=0)
+        {
+            x /= 10;
+            ++count;
+        }
+        printf("%d has digit of: %d\n",num,count);
     return 0;
 }
