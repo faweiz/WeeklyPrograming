808a50b0be4f76c86720f496e32a03e81731a1c8
Faweiz <faweiz@gmail.com>
2015-05-02 09:32:32 -0400

    1. Hello World!

--- 1_HelloWorld/1_HelloWorld.c
+++ 1_HelloWorld/1_HelloWorld.c
@@ -1,5 +1,10 @@
-//C语言：每周1程序01: Hello World
-//https://github.com/faweiz
+// C:Weekly programming 01: Hello World
+// https://github.com/faweiz
+// https://github.com/faweiz
+// https://portfolium.com/faweiz
+// https://www.linkedin.com/in/faweiz
+// First C program Hello World!
+
 #include <stdio.h>
 int main(){
 	printf("Hello World!\n");
