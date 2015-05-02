b873b6b89493fcdb90ad978c03924655008394f1
Faweiz <faweiz@gmail.com>
2015-05-02 09:36:54 -0400

    6. For Loop

--- 6_For_loop/6_For_loop.c
+++ 6_For_loop/6_For_loop.c
@@ -1,5 +1,8 @@
-//C:Weekly programming 06: For Loop
-//https://github.com/faweiz
+// C:Weekly programming 06: For Loop
+// https://github.com/faweiz
+// https://portfolium.com/faweiz
+// https://www.linkedin.com/in/faweiz
+
 #include<stdio.h>
 int main()
 {
