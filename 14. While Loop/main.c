27ffb37a2c57967f091575280b88e6c4147b4fe1
Faweiz <faweiz@gmail.com>
2015-05-02 10:03:39 -0400

    14. While Loop

--- 14. While Loop/main.c
+++ 14. While Loop/main.c
@@ -1,8 +1,20 @@
+// C:Weekly programming 14: While Loop
+// https://github.com/faweiz
+// https://portfolium.com/faweiz
+// https://www.linkedin.com/in/faweiz
+// Find the Sum of number from 1 to 100
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int i = 1;
+    int sum =0;
+    while(i<=100){
+        sum += i;
+        i++;
+    }
+    printf("The sum of 1 to 100 = %d\n",sum);
     return 0;
 }
