8a5b651a80e60eaa08384a11edaabe139f60c303
Faweiz <faweiz@gmail.com>
2015-05-03 11:07:34 -0400

    27. Count Coin

--- 27. Count Coin/main.c
+++ 27. Count Coin/main.c
@@ -4,16 +4,51 @@
 // https://www.linkedin.com/in/faweiz
 // Designer: Fawei Zhang
 // Date: 5/2/2015
+// Prints the number of dollars, quarters, dimes, nickels and pennies on output
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    float number;
+         float money = 0.000;
+
+         printf("Enter the number of money: $");
+         scanf("%f",&money);
+
+         int dollar = 100;
+         int quarter = 25;
+         int dime = 10;
+         int nickel = 5;
+         int penny = 1;
+
+         int cents = money*100.000;
+         int totalDollar = (cents / dollar);
+         cents %= dollar;
+         int totalQuarter = (cents / quarter);
+         cents %= quarter;
+         int totalDime = (cents / dime);
+         cents %= dime;
+         int totalNickel = (cents / nickel);
+         cents %= nickel;
+         int totalPenny = (cents / penny);
+         cents %= penny;
+
+         // Print the total number of Pennies + Nickels + Dimes + Quarters + Dollars = Money
+         printf("%d Dollars + %d Quarters + %d Dimes + %d Nickels + %d Pennies to get $%.2lf\n", totalDollar,totalQuarter,totalDime,totalNickel,totalPenny,money);
+
+
+         return 0;
+}
+
+
+/*
+int main()
+{
+    double number = 0.0;
     int penny,nickel,dime,quarter,dollar;
     printf("Enter the amount of Dollar: ");
-    scanf("%f",&number);
+    scanf("%lf",&number);
    for(penny=0.1;penny<number*10;penny++)
     {
         for(nickel=0.5; nickel<number*10/0.5;nickel++)
@@ -26,7 +61,7 @@ int main()
                     {
                         if(penny*0.1 + nickel*0.5 + dime*1 + quarter*2.5 == number *10)
                         {
-                            printf("%d penny + %d nickel + %d dime + %d quarter to get $%f\n", penny,nickel,dime,quarter,number);
+                            printf("%d penny + %d nickel + %d dime + %d quarter to get $%lf\n", penny,nickel,dime,quarter,number);
                         }
                     }
                     else if(number>10)
@@ -35,7 +70,7 @@ int main()
                     {
                         if(penny*0.1 + nickel*0.5 + dime*1 + quarter*2.5 + dollar*10 == number *10)
                         {
-                            printf("%d penny + %d nickel + %d dime + %d quarter + %d dollar to get %f\n", penny,nickel,dime,quarter,dollar,number);
+                            printf("%d penny + %d nickel + %d dime + %d quarter + %d dollar to get %lf\n", penny,nickel,dime,quarter,dollar,number);
                         }
                     }
                     }
@@ -47,30 +82,7 @@ int main()
     }
     return 0;
 }
-
-
-
-  /*
-        for(quarter=2.5;quarter<number*10/2.5;quarter++)
-        {
-                if(one + quarter*2.5 == number*10)
-                {
-                    printf("Make up of %d cents with %d quarter and to get $%d\n",one,quarter,number);
-                }
-
-        }
-    }
-
-    return 0;
-}
 */
-
-
-
-
-
-
-
 
 /*
 #include <stdio.h>
@@ -99,3 +111,4 @@ int main()
     return 0;
 }
 */
+
