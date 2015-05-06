4f69c2e6a4a2b441ebc9309889e18009a43c20bf
Faweiz <faweiz@gmail.com>
2015-05-06 19:21:53 -0400

    28. Length of a message

---
+++ 28. Length of a message/main.c
@@ -0,0 +1,40 @@
+// C:Weekly programming 28. Length of a message
+// https://github.com/faweiz
+// https://portfolium.com/faweiz
+// https://www.linkedin.com/in/faweiz
+// Determines the length of a message
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main()
+{
+    char ch;
+    int len = 0;
+
+    printf("Enter the message: ");
+    ch = getchar();
+    while(ch!='\n')
+    {
+        len++;
+        ch = getchar();
+    }
+    printf("Your message was %d character(s) long. \n",len);
+    return 0;
+}
+*/
+
+// To be shortened
+
+int main()
+{
+    int len = 0;
+
+    printf("Enter the message: ");
+    while(getchar() != '\n')
+    {
+        len++;
+    }
+    printf("Your message was %d character(s) long. \n",len);
+    return 0;
+}
