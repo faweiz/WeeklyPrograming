9155315ee5a032fc568df5604bc9627eaa314da6
Faweiz <faweiz@gmail.com>
2015-05-06 19:36:41 -0400

    29. Diamond Pattern

---
+++ 29. Diamond Shape/main.c
@@ -0,0 +1,44 @@
+// C:Weekly programming 29. Diamond Pattern
+// https://github.com/faweiz
+// https://portfolium.com/faweiz
+// https://www.linkedin.com/in/faweiz
+// Print diamond pattern of starts
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,k,shape=1;
+    printf("Enter number of rows: \n");
+    scanf("%d",&i);
+    shape = i - 1;
+    for(k=1;k<=i;k++)
+    {
+        for(j=1;j<=shape;j++)
+        {
+            printf(" ");
+        }
+            shape--;
+        for(j=1;j<2*k-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    shape = 1;
+    for(k=1;k<=i-1;k++)
+    {
+        for(j=1;j<=shape;j++)
+        {
+            printf(" ");
+        }
+        shape++;
+        for(j=1;j<=2*(i-k)-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
