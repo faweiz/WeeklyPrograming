bd1e48618b20e1201aaae5a5391b0798215f6ff8
Faweiz <faweiz@gmail.com>
2015-05-06 23:22:53 -0400

    7: 9 by 9 arithmetic function

--- 7_9x9/main.c
+++ 7_9x9/main.c
@@ -2,6 +2,8 @@
 // https://github.com/faweiz
 // https://portfolium.com/faweiz
 // https://www.linkedin.com/in/faweiz
+// Designer: Fawei Zhang
+// Date: 5/6/2015
 // Array of 9*9 arithmetic function
 
 #include <stdio.h>
